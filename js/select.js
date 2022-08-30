9e5d6a6786ff5b007c303fd9696e64950bd624ae
Sakib <ucs14568@gmail.com>
2022-08-30 23:41:03 +0600

    added calculate file

--- js/select.js
+++ js/select.js
@@ -1,7 +1,13 @@
-document.getElementById('btn-select-mash').addEventListener('click', function(){
-   const selectMash = document.getElementById('btn-mash');
-   const selectMashString = selectMash.value;
-   const previousSelectMash = parseFloat(selectMashString);
-   const newSelectMash = previousSelectMash + '';
-   selectMash.value = newSelectMash;
+// document.getElementById('btn-select-mash').addEventListener('click', function(){
+//    const selectMash = document.getElementById('btn-mash');
+//    const selectMashString = selectMash.value;
+//    const previousSelectMash = parseFloat(selectMashString);
+//    const newSelectMash = previousSelectMash + '';
+//    selectMash.value = newSelectMash;
+// })
+
+document.getElementById('btn-per-player').addEventListener('click', function(){
+    const budegtPerPlayer = document.getElementById('btn-per-player');
+    const budgetPlayer = budegtPerPlayer * 1;
+    console.log(budgetPlayer);
 })
